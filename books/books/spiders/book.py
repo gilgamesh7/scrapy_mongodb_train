945913cd75f2f0463ea1a35d1c8b10839d32db95
gilgamesh7 <felidaesrule@yahoo.com>
2025-07-22 20:41:33 +1200

    Recursively follow all "Next" links

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -16,6 +16,7 @@ class BookSpider(scrapy.Spider):
             item["price"] = book.css(".price_color::text").get()
             yield item
 
-        # next_page = response.css("li.next a::attr(href)").get()
-        # if next_page:
-        #     yield response.follow(next_page, self.parse)
+        next_page = response.css("li.next > a::attr(href)").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield response.follow(url=next_page_url, callback=self.parse)
